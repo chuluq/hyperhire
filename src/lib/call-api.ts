157c8955505e7b3b1c4fc39ce1c1d772de4062fb
chuluq <chuluq@arvis.id>
2024-11-22 16:49:37 +0700

    chore: add env

--- src/lib/call-api.ts
+++ src/lib/call-api.ts
@@ -1,33 +1,33 @@
 export async function getFooter() {
-  const data = await fetch('http://localhost:3000/api/footer');
+  const data = await fetch(`${process.env.API_URL}/api/footer`);
   const footer = await data.json();
 
   return footer;
 }
 
 export async function getCard() {
-  const data = await fetch('http://localhost:3000/api/card');
+  const data = await fetch(`${process.env.API_URL}/api/card`);
   const card = await data.json();
 
   return card;
 }
 
 export async function getTextIcon() {
-  const data = await fetch('http://localhost:3000/api/text-icon');
+  const data = await fetch(`${process.env.API_URL}/api/text-icon`);
   const textIcon = await data.json();
 
   return textIcon;
 }
 
 export async function getBoxInfo() {
-  const data = await fetch('http://localhost:3000/api/box-info');
+  const data = await fetch(`${process.env.API_URL}/api/box-info`);
   const boxInfo = await data.json();
 
   return boxInfo;
 }
 
 export async function getCheckbox() {
-  const data = await fetch('http://localhost:3000/api/checkbox');
+  const data = await fetch(`${process.env.API_URL}/api/checkbox`);
   const checkbox = await data.json();
 
   return checkbox;
